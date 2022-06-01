73894ec3f96e334ab549a529752d48391816a468
John Komninos <johnkomninos@johns-air.myfiosgateway.com>
2022-06-01 16:10:49 -0400

    added edit button functionality

--- src/components/showbeach.js
+++ src/components/showbeach.js
@@ -6,13 +6,16 @@ const ShowBeach = (props) =>{
       location:{props.beach.location}<br/>
       Popularity:{props.beach.popularity}<br/>
       <button onClick={(event)=>{props.handleDelete(props.beach)}}>Remove Beach</button>
+      {props.creatureID !== props.beach._id ?
+      <button onClick={(event)=>{props.toggleEdit(props.beach)}}>Edit Beach</button> : <button onClick={props.closeEdit}>Close</button>}
+      {props.creatureID === props.beach._id ?
       <form onSubmit={(event)=>{props.handleUpdate(props.beach,   event)}}>
-        Name: <input type ="text" onChange={props.handleName} placeholder={props.beach.name}/><br/>
-        Image:<input type ="text" onChange={props.handleImage}placeholder={props.beach.image}/><br/>
-        Location:<input type ="text" onChange={props.handleLocation}placeholder={props.beach.location}/><br/>
-        Popularity:<input type ="text" onChange={props.handlePopularity}placeholder={props.beach.popularity}/><br/>
-        <input type="submit" value="Edit Beach"/>
-      </form>
+          Name: <input type ="text" onChange={props.handleUpdateName}/><br/>
+          Image:<input type ="text" onChange={props.handleUpdateImage}/><br/>
+          Location:<input type ="text" onChange={props.handleUpdateLocation}/><br/>
+          Popularity:<input type ="text" onChange={props.handleUpdatePopularity}/><br/>
+        <input type="submit" value="Submit"/>
+      </form> : ""}
     </div>
   )
 }
