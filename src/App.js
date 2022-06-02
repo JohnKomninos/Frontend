f5f7adcfe4cc04ba20f3f5190b524e1406670a9f
CANDACE HARRIS <candaceharris@CANDACEs-MBP.lan>
2022-06-02 14:07:30 -0400

    updated css

--- src/App.js
+++ src/App.js
@@ -7,144 +7,168 @@ import ShowBeach from "./components/showbeach";
 // import NewComment from "./components/newComment";
 
 function App() {
-  const lcl = `http://localhost:3000/`
-  const hrk = `https://mysterious-meadow-36213.herokuapp.com/`
+  const lcl = `http://localhost:3000/`;
+  const hrk = `https://mysterious-meadow-36213.herokuapp.com/`;
 
-//setting all the states
-  const [name, setName] = useState()
-  const [image, setImage] = useState()
-  const [location, setLocation] = useState()
-  const [popularity, setPopularity] = useState()
-  const [updateName, setUpdateName] = useState()
-  const [updateImage, setUpdateImage] = useState()
-  const [updateLocation, setUpdateLocation] = useState()
-  const [updatePopularity, setUpdatePopularity] = useState()
+  //setting all the states
+  const [name, setName] = useState();
+  const [image, setImage] = useState();
+  const [location, setLocation] = useState();
+  const [popularity, setPopularity] = useState();
+  const [updateName, setUpdateName] = useState();
+  const [updateImage, setUpdateImage] = useState();
+  const [updateLocation, setUpdateLocation] = useState();
+  const [updatePopularity, setUpdatePopularity] = useState();
 
-
-  const [index, setIndex] = useState(0)
-  const [creatureID, setCreatureID] = useState()
-  const [showForm, setShowForm] = useState()
-  const [beach, setBeach] = useState([])
-
-//function to handle name input
-  const handleName = (event) =>{
-    setName(event.target.value)
-  }
-
-//function to handle image input
-  const handleImage = (event) =>{
-    setImage(event.target.value)
-  }
-
-//function to handle location input
-  const handleLocation = (event) =>{
-    setLocation(event.target.value)
-  }
-
-//function to handle popularity input
-  const handlePopularity = (event) =>{
-    setPopularity(event.target.value)
-  }
+  const [index, setIndex] = useState(0);
+  const [creatureID, setCreatureID] = useState();
+  const [showForm, setShowForm] = useState();
+  const [beach, setBeach] = useState([]);
 
   //function to handle name input
-    const handleUpdateName = (event) =>{
-      setUpdateName(event.target.value)
-    }
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
 
   //function to handle image input
-    const handleUpdateImage = (event) =>{
-      setUpdateImage(event.target.value)
-    }
+  const handleImage = (event) => {
+    setImage(event.target.value);
+  };
 
   //function to handle location input
-    const handleUpdateLocation = (event) =>{
-      setUpdateLocation(event.target.value)
-    }
+  const handleLocation = (event) => {
+    setLocation(event.target.value);
+  };
 
   //function to handle popularity input
-    const handleUpdatePopularity = (event) =>{
-      setUpdatePopularity(event.target.value)
-    }
+  const handlePopularity = (event) => {
+    setPopularity(event.target.value);
+  };
 
+  //function to handle name input
+  const handleUpdateName = (event) => {
+    setUpdateName(event.target.value);
+  };
 
-//this displays our database on page load
-  useEffect(()=>{
-    axios.get(lcl).then((response)=>{
-      setBeach(response.data)
-    })
-  },[])
+  //function to handle image input
+  const handleUpdateImage = (event) => {
+    setUpdateImage(event.target.value);
+  };
 
-//lets users add new beaches
-  const submitBeach = (event) =>{
-    event.preventDefault()
-    setIndex(index+1)
-    axios.post(lcl, {
-      name:name,
-      image:image,
-      location:location,
-      popularity:popularity,
-      index:index
-    }).then(()=>{
-      axios.get(lcl).then((response)=>{
-        setBeach(response.data)
-      })
-    })
-  }
+  //function to handle location input
+  const handleUpdateLocation = (event) => {
+    setUpdateLocation(event.target.value);
+  };
 
-//lets users delete beaches
-  const handleDelete = (beachData) =>{
-    const lclID = `http://localhost:3000/${beachData._id}`
-    const hrkID = `https://mysterious-meadow-36213.herokuapp.com/${beachData._id}`
-    axios.delete(lclID).then(()=>{
-      axios.get(lcl).then((response)=>{
-        setBeach(response.data)
-      })
-    })
-  }
+  //function to handle popularity input
+  const handleUpdatePopularity = (event) => {
+    setUpdatePopularity(event.target.value);
+  };
 
-  const handleUpdate =(beachData, event)=>{
-    const lclID = `http://localhost:3000/${beachData._id}`
-    const hrkID = `https://mysterious-meadow-36213.herokuapp.com/${beachData._id}`
-    event.preventDefault()
-    setCreatureID()
-    axios.put(lclID ,{
-      name:updateName,
-      image:updateImage,
-      location:updateLocation,
-      popularity:updatePopularity
-    }).then(()=>{
-      axios.get(lcl).then((response)=>{
-        setBeach(response.data)
+  //this displays our database on page load
+  useEffect(() => {
+    axios.get(lcl).then((response) => {
+      setBeach(response.data);
+    });
+  }, []);
+
+  //lets users add new beaches
+  const submitBeach = (event) => {
+    event.preventDefault();
+    setIndex(index + 1);
+    axios
+      .post(lcl, {
+        name: name,
+        image: image,
+        location: location,
+        popularity: popularity,
+        index: index,
       })
       .then(() => {
         axios.get(lcl).then((response) => {
           setBeach(response.data);
         });
       });
-  }
+  };
 
-  const toggleEdit = (beachData) =>{
-    setCreatureID(beachData._id)
-    setUpdateName()
-    setUpdateImage()
-    setUpdateLocation()
-    setUpdatePopularity()
-  }
+  //lets users delete beaches
+  const handleDelete = (beachData) => {
+    const lclID = `http://localhost:3000/${beachData._id}`;
+    const hrkID = `https://mysterious-meadow-36213.herokuapp.com/${beachData._id}`;
+    axios.delete(lclID).then(() => {
+      axios.get(lcl).then((response) => {
+        setBeach(response.data);
+      });
+    });
+  };
 
-  const closeEdit = () =>{
-    setCreatureID()
-  }
+  const handleUpdate = (beachData, event) => {
+    const lclID = `http://localhost:3000/${beachData._id}`;
+    const hrkID = `https://mysterious-meadow-36213.herokuapp.com/${beachData._id}`;
+    event.preventDefault();
+    setCreatureID();
+    axios
+      .put(lclID, {
+        name: updateName,
+        image: updateImage,
+        location: updateLocation,
+        popularity: updatePopularity,
+      })
+      .then(() => {
+        axios.get(lcl).then((response) => {
+          setBeach(response.data);
+        });
+      });
+  };
 
-  const toggleForm = () =>{
-    setShowForm(true)
-  }
-// Created newBeach component for adding beaches to the data base
+  const toggleEdit = (beachData) => {
+    setCreatureID(beachData._id);
+    setUpdateName();
+    setUpdateImage();
+    setUpdateLocation();
+    setUpdatePopularity();
+  };
+
+  const closeEdit = () => {
+    setCreatureID();
+  };
+
+  const toggleForm = () => {
+    setShowForm(true);
+  };
+  // Created newBeach component for adding beaches to the data base
   return (
     <>
-      <NewBeach handleName={handleName} handleImage={handleImage} handleLocation={handleLocation} handlePopularity={handlePopularity} submitBeach={submitBeach} toggleForm={toggleForm} showForm={showForm}/>
+      <NewBeach
+        handleName={handleName}
+        handleImage={handleImage}
+        handleLocation={handleLocation}
+        handlePopularity={handlePopularity}
+        submitBeach={submitBeach}
+        toggleForm={toggleForm}
+        showForm={showForm}
+      />
       <div className="flex-parent">
-        {beach.map((beach)=>{
-          return <ShowBeach beach={beach} handleDelete={handleDelete} handleName={handleName} handleImage={handleImage} handleLocation={handleLocation} handlePopularity={handlePopularity} handleUpdate={handleUpdate} handleUpdateName={handleUpdateName} handleUpdateImage={handleUpdateImage} handleUpdateLocation={handleUpdateLocation} handleUpdatePopularity={handleUpdatePopularity} index={index} creatureID={creatureID} toggleEdit={toggleEdit} closeEdit={closeEdit}/>
+        {beach.map((beach) => {
+          return (
+            <ShowBeach
+              beach={beach}
+              handleDelete={handleDelete}
+              handleName={handleName}
+              handleImage={handleImage}
+              handleLocation={handleLocation}
+              handlePopularity={handlePopularity}
+              handleUpdate={handleUpdate}
+              handleUpdateName={handleUpdateName}
+              handleUpdateImage={handleUpdateImage}
+              handleUpdateLocation={handleUpdateLocation}
+              handleUpdatePopularity={handleUpdatePopularity}
+              index={index}
+              creatureID={creatureID}
+              toggleEdit={toggleEdit}
+              closeEdit={closeEdit}
+            />
+          );
         })}
       </div>
     </>
--- src/components/showbeach.js
+++ src/components/showbeach.js
@@ -1,54 +1,79 @@
 const ShowBeach = (props) => {
   return (
-    <div className="child-div">
-      Name:{props.beach.name}
-      <br />
-      Image:{props.beach.image}
-      <br />
-      location:{props.beach.location}
-      <br />
-      Popularity:{props.beach.popularity}
-      <br />
-      <button
-        onClick={(event) => {
-          props.handleDelete(props.beach);
-        }}
-      >
-        Remove Beach
-      </button>
-      {props.creatureID !== props.beach._id ? (
-        <button
-          onClick={(event) => {
-            props.toggleEdit(props.beach);
-          }}
-        >
-          Edit Beach
-        </button>
-      ) : (
-        <button onClick={props.closeEdit}>Close</button>
-      )}
-      {props.creatureID === props.beach._id ? (
-        <form
-          onSubmit={(event) => {
-            props.handleUpdate(props.beach, event);
-          }}
-        >
-          Name: <input type="text" onChange={props.handleUpdateName} />
-          <br />
-          Image:
-          <input type="text" onChange={props.handleUpdateImage} />
-          <br />
-          Location:
-          <input type="text" onChange={props.handleUpdateLocation} />
-          <br />
-          Popularity:
-          <input type="text" onChange={props.handleUpdatePopularity} />
-          <br />
-          <input type="submit" value="Submit" />
-        </form>
-      ) : (
-        ""
-      )}
+    <div className="container-fluid">
+      {/* <div className="card"> */}
+      <div className="row">
+        <div className="col-12 mt-3">
+          <div class="card">
+            <div class="card-horizontal">
+              <div class="img-square-wrapper">
+                <img class="" src={props.beach.image} alt="card image cap" />
+
+                <div className="card-body">
+                  <h2 className="card-title mt-2">
+                    {" "}
+                    Name:{props.beach.name}
+                    <br />{" "}
+                  </h2>
+                  <p className="card-text">
+                    location:{props.beach.location}
+                    <br />
+                    Popularity:{props.beach.popularity}
+                    <br />
+                    <button
+                      onClick={(event) => {
+                        props.handleDelete(props.beach);
+                      }}
+                    >
+                      Remove Beach
+                    </button>
+                    {props.creatureID !== props.beach._id ? (
+                      <button
+                        onClick={(event) => {
+                          props.toggleEdit(props.beach);
+                        }}
+                      >
+                        Edit Beach
+                      </button>
+                    ) : (
+                      <button onClick={props.closeEdit}>Close</button>
+                    )}
+                    {props.creatureID === props.beach._id ? (
+                      <form
+                        onSubmit={(event) => {
+                          props.handleUpdate(props.beach, event);
+                        }}
+                      >
+                        Name:{" "}
+                        <input type="text" onChange={props.handleUpdateName} />
+                        <br />
+                        Image:
+                        <input type="text" onChange={props.handleUpdateImage} />
+                        <br />
+                        Location:
+                        <input
+                          type="text"
+                          onChange={props.handleUpdateLocation}
+                        />
+                        <br />
+                        Popularity:
+                        <input
+                          type="text"
+                          onChange={props.handleUpdatePopularity}
+                        />
+                        <br />
+                        <input type="submit" value="Submit" />
+                      </form>
+                    ) : (
+                      ""
+                    )}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
