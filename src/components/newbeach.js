5199dad50e3d06a0fe349919b946258cb880028b
John Komninos <johnkomninos@johns-air.myfiosgateway.com>
2022-06-04 16:20:59 -0400

    saving

--- src/components/newbeach.js
+++ src/components/newbeach.js
@@ -1,63 +1,18 @@
-const NewBeach = (props) => {
-  return (
+const NewBeach = (props) =>{
+  return(
     <div>
-      <nav> Healthy Travel Tips</nav>
-      <header className="header">
-        <h1> Beaches around the World</h1>
-        <p className="intro">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </header>
-      {props.showForm !== true ? (
-        <button
-          type="button"
-          class="btn btn-primary btn-lg"
-          onClick={props.toggleForm}
-        >
-          Add Beach
-        </button>
-      ) : (
-        <button onClick={props.toggleForm}>Close</button>
-      )}
-      {props.showForm ? (
-        <form onSubmit={props.submitBeach}>
-          Name: <input type="text" onChange={props.handleName} required />
-          <br />
-          Image:
-          <input type="text" onChange={props.handleImage} required />
-          <br />
-          Location:
-          <input type="text" onChange={props.handleLocation} required />
-          <br />
-          Popularity:
-          <input type="text" onChange={props.handlePopularity} required />
-          <br />
-          <input type="submit" value="submit" />
-        </form>
-      ) : (
-        ""
-      )}
+      <h3>Beaches</h3>
+      {props.showForm !== true ? <button onClick={props.toggleForm}>Add Beach</button> : <button onClick={props.toggleForm}>Close</button>}
+      { props.showForm ?
+      <form onSubmit={props.submitBeach}>
+        Name: <input type ="text" onChange={props.handleName} required/><br/>
+        Image:<input type ="text" onChange={props.handleImage} required/><br/>
+        Location:<input type ="text" onChange={props.handleLocation} required/><br/>
+        Popularity:<input type ="text" onChange={props.handlePopularity} required/><br/>
+        <input type="submit" value="submit"/>
+      </form> : "" }
     </div>
-  );
-};
+  )
+}
 
-export default NewBeach;
+export default NewBeach
